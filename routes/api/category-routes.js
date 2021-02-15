6fe291596debe471169695cf7bc235c1b28ddc3e
quaylas <quayla@cxadvisors.com>
2021-02-14 17:34:27 -0700

    updates to config and models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,56 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [{
+      model: Product
+    }]
+  })
+  .then(dbCategories => {
+    res.json(dbCategories);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{model: Product}]
+  })
+  .then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where:{
+      id: req.body.id
+    }
+  }).then(dbCategory =>{
+    res.json(dbCategory);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where:{
+      id: req.params.id
+    }
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 module.exports = router;
